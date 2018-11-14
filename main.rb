6f33f24ceb336543cd89b2bdbedc02d868f89a09
Adam Shannon <adamkshannon@gmail.com>
2018-11-13 19:55:50 -0600

    all: code is Apache 2 licensed

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
 # coding: utf-8
+# Copyright 2018 The Moov Authors
+# Use of this source code is governed by an Apache License
+# license that can be found in the LICENSE file.
 require 'json'
 require 'net/http'
 require 'rainbow'
